c994614915685421a595f2d90306bbd0e36550a5
Hércules Natan <medeiroshercules23@gmail.com>
2024-11-13 12:02:47 -0300

    [Questionário] Entrada/Saída de Dados

--- QUESTIONÁRIO 2/q1.py
+++ QUESTIONÁRIO 2/q1.py
@@ -1,9 +1,9 @@
-entrada = str(input())
+entrada = (input())
 
 primeira_saida = f'{entrada}'
 segunda_saida = f'{entrada*2}'
 terceira_saida = f'{entrada} {entrada}'
-quarta_saida = f'2{entrada}'
+quarta_saida = f'2{str(entrada)}'
 quinta_saida = f'[{entrada}]'
 
 print(primeira_saida, segunda_saida, terceira_saida, quarta_saida, quinta_saida, sep="\n")
