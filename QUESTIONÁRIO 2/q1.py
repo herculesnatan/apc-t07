601b78e80910ab8c8b35f6b741dd31c6d46bac32
Hércules Natan <medeiroshercules23@gmail.com>
2024-11-13 12:00:30 -0300

    [Questionário] Variáveis, expressões e instruções

---
+++ QUESTIONÁRIO 2/q1.py
@@ -0,0 +1,9 @@
+entrada = str(input())
+
+primeira_saida = f'{entrada}'
+segunda_saida = f'{entrada*2}'
+terceira_saida = f'{entrada} {entrada}'
+quarta_saida = f'2{entrada}'
+quinta_saida = f'[{entrada}]'
+
+print(primeira_saida, segunda_saida, terceira_saida, quarta_saida, quinta_saida, sep="\n")
